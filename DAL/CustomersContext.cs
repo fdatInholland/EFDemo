9185af8b1f706b80ef08eccf3a62c26c2b179c3a
Frank Dersjant <36342252+frankdersjant@users.noreply.github.com>
2025-09-09 09:03:41 +0200

    Add project files.

---
+++ DAL/CustomersContext.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+
+namespace DAL
+{
+    public class CustomersContext : DbContext
+    {
+        public DbSet<Customer> Customer { get; set;  }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeRole> EmployeeRoles { get; set; }
+
+        public CustomersContext(DbContextOptions<CustomersContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasIndex(e => e.CustomerID)
+                .HasDatabaseName("CustomerCode")
+                .IsUnique();
+
+                entity.HasMany(d => d.Invoice);
+
+
+                entity.Property(e => e.FirstName)
+                .HasColumnType("varchar(60)");
+            });
+        }
+    }
+}
---
+++ DAL/Migrations/20250908122637_initial-create.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CustomerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "varchar(60)", maxLength: 60, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.CustomerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeRoles",
+                columns: table => new
+                {
+                    EmployeeRoleID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeRoles", x => x.EmployeeRoleID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    InvoiceID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerID = table.Column<int>(type: "int", nullable: false),
+                    InvoiceDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.InvoiceID);
+                    table.ForeignKey(
+                        name: "FK_Invoices_Customer_CustomerID",
+                        column: x => x.CustomerID,
+                        principalTable: "Customer",
+                        principalColumn: "CustomerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeEmployeeRole",
+                columns: table => new
+                {
+                    EmployeesEmployeeID = table.Column<int>(type: "int", nullable: false),
+                    employeeRolesEmployeeRoleID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeEmployeeRole", x => new { x.EmployeesEmployeeID, x.employeeRolesEmployeeRoleID });
+                    table.ForeignKey(
+                        name: "FK_EmployeeEmployeeRole_EmployeeRoles_employeeRolesEmployeeRoleID",
+                        column: x => x.employeeRolesEmployeeRoleID,
+                        principalTable: "EmployeeRoles",
+                        principalColumn: "EmployeeRoleID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeEmployeeRole_Employees_EmployeesEmployeeID",
+                        column: x => x.EmployeesEmployeeID,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InvoiceLine",
+                columns: table => new
+                {
+                    InvoiceLineID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InvoiceID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvoiceLine", x => x.InvoiceLineID);
+                    table.ForeignKey(
+                        name: "FK_InvoiceLine_Invoices_InvoiceID",
+                        column: x => x.InvoiceID,
+                        principalTable: "Invoices",
+                        principalColumn: "InvoiceID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "CustomerCode",
+                table: "Customer",
+                column: "CustomerID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeEmployeeRole_employeeRolesEmployeeRoleID",
+                table: "EmployeeEmployeeRole",
+                column: "employeeRolesEmployeeRoleID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoiceLine_InvoiceID",
+                table: "InvoiceLine",
+                column: "InvoiceID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_CustomerID",
+                table: "Invoices",
+                column: "CustomerID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeEmployeeRole");
+
+            migrationBuilder.DropTable(
+                name: "InvoiceLine");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeRoles");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Invoices");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
